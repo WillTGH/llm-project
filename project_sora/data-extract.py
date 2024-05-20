bf3c8aed8a5b0072d62a8b177e81da3f14cf675a
WillTGH <167842272+WillTGH@users.noreply.github.com>
2024-05-20 16:20:56 +0800

    Add files via upload

--- project_sora/data-extract.py
+++ project_sora/data-extract.py
@@ -1,6 +1,7 @@
 import os
 import lzma
 from tqdm import tqdm
+import time
 
 def xz_files_in_dir(directory):
     files = []
@@ -9,31 +10,50 @@ def xz_files_in_dir(directory):
             files.append(filename)
     return files
 
-folder_path = "V:/corpus_datasets/openwebtext"
-output_file = "output{}.txt"
+folder_path = "V:/llm-project/datasets/openwebtext"
+output_file_train = "train_split.txt"
+output_file_val = "val_split.txt"
 vocab_file = "vocab.txt"
-split_files = int(input("How many split?"))
 
 files = xz_files_in_dir(folder_path)
 
 total_files = len(files)
 
-max_count = total_files // split_files if split_files != 0 else total_files
+split_index = int(total_files * 0.9) # 90% for training
+files_train = files[:split_index]
+files_val = files[split_index:]
 
 vocab = set()
 
-for i in range(split_files):
-    with open(output_file.format(i), "w", encoding="utf-8") as outfile:
-        for count, filename in enumerate(tqdm(file[:max_count], total=max_count)):
-            if count >= max_count:
-                break
-            file_path = os.path.join(folder_path)
-            with lzma.open(file_path, "rt", encoding="utf-8") as infile:
-                text = infile.read()
-                outfile.write(text)
-                characters = set(text)
-                vocab.update(characters)
-        files = files[max_count:]
+start_time = time.time()
+
+with open(output_file_train.format(i), "w", encoding="utf-8") as outfile:
+    for filename in tqdm(files_train, total=len(files_train)):
+        file_path = os.path.join(folder_path)
+        with lzma.open(file_path, "rt", encoding="utf-8") as infile:
+            text = infile.read()
+            outfile.write(text)
+            characters = set(text)
+            vocab.update(characters)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"{elapsed_time:.8f}")
+
+start_time = time.time()
+
+with open(output_file_val.format(i), "w", encoding="utf-8") as outfile:
+    for filename in tqdm(files_val, total=len(files_val)):
+        file_path = os.path.join(folder_path)
+        with lzma.open(file_path, "rt", encoding="utf-8") as infile:
+            text = infile.read()
+            outfile.write(text)
+            characters = set(text)
+            vocab.update(characters)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"{elapsed_time:.8f}")
 
 with open(vocab_file, "w", encoding="utf-8") as vfile:
     for char in vocab:
